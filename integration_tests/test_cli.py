be5528d5f593bdde73f455eacf9f07744e4c9649
Yann <yannforget@mailbox.org>
2021-02-24 16:50:09 +0100

    Add CLI tests

---
+++ integration_tests/test_cli.py
@@ -0,0 +1,36 @@
+"""Tests for CLI."""
+
+from click.testing import CliRunner
+
+from landsatxplore.cli import cli
+
+
+def test_search():
+    runner = CliRunner()
+    result = runner.invoke(
+        cli,
+        [
+            "search",
+            "--dataset",
+            "landsat_tm_c1",
+            "--location",
+            "12.53",
+            "-1.53",
+            "--start",
+            "1995-01-01",
+            "--end",
+            "1995-12-31",
+        ],
+    )
+    assert result.exit_code == 0
+    assert "LT51950511995219MPS00" in result.output
+
+
+def test_download():
+    runner = CliRunner()
+    result = runner.invoke(
+        cli,
+        ["download", "--dataset", "landsat_tm_c1", "--skip", "LT51950511995219MPS00"],
+    )
+    assert result.exit_code == 0
+    assert "LT05_L1TP_195051_19950807_20170107_01_T1.tar.gz" in result.output
